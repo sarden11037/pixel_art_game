3b1609e740b4bf5c05df01320129a4be1a73f684
sarden11037 <severo.7477@gmail.com>
2017-12-14 14:20:23 -0600

    Text updates
    completed updates to text, added new google fonts, and a responsive bootstrap button

---
+++ design.js
@@ -0,0 +1,12 @@
+//select color input 
+//select size input 
+
+//when size is submitted by the user, call make grid 
+function makeGrid() {
+    "use strict"; 
+// opening js code 
+   
+    
+}
+
+makeGrid();
