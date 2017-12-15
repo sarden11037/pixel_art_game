549ede3b38063175c66b970b77a46f435c2e7720
sarden11037 <severo.7477@gmail.com>
2017-12-15 09:35:46 -0600

    functionality 1.0
    added jQuery selctors for color picker.

--- design.js
+++ design.js
@@ -1,12 +1,20 @@
-//select color input 
+//select color input ;
+let colorChoice;
+colorChoice = $("#colorPicker").val();
+alert(colorChoice);
+//event listener for color choice 
+$("#colorPicker").change(function() {
+  colorChoice = $("#colorPicker").val();
+  });
+$("#colorPicker").val("colorChoice");                                       
 //select size input 
 
 //when size is submitted by the user, call make grid 
-function makeGrid() {
-    "use strict"; 
-// opening js code 
+/*function makeGrid() {
+ 
    
     
 }
 
 makeGrid();
+*/
