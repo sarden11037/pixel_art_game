b2a85f86e396256fcf5b05715f332ba16791d6c3
sarden11037 <severo.7477@gmail.com>
2017-12-15 14:17:58 -0600

    working functionality
    working functionality commit of all selectors, event listeners, and grod sizing. adding customization

--- design.js
+++ design.js
@@ -1,20 +1,47 @@
 //select color input ;
 let colorChoice;
 colorChoice = $("#colorPicker").val();
-alert(colorChoice);
+//alert(colorChoice);
 //event listener for color choice 
 $("#colorPicker").change(function() {
   colorChoice = $("#colorPicker").val();
   });
-$("#colorPicker").val("colorChoice");                                       
+$("#colorPicker").val("colorChoice");
 //select size input 
-
 //when size is submitted by the user, call make grid 
-/*function makeGrid() {
+/*
+$("submit-Size").change(function(e) {
+    e.preventDefault();
+});*/
+$("#submit-Size").click(function(e) { 
+                         makeGrid(e);
+                         });
+function makeGrid(e) {
  
-   
+    e.preventDefault();
+    alert("first run");
+    let height = $("#input_height").val();
+    alert(height + "height");
+    let width = $("#input_width").val();
+    alert("width" + width);
     
-}
+    for (let i = 0;i< height;i++)
+        {
+            $("#pixel_canvas").append("<tr></tr>");
+            for(let x = 0; x< width;x++)
+                {
+                    $("tr").last().append("<td></td>");
+                }
+        }
+    
+  }
+let click = $("#pixel_canvas").on("click", "td", function() {
+  
+  $(this).css("background-color", colorChoice);
+});
+//was thinging about adding an if statement
+//for errors on clicking but was uneccsary
+    
 
-makeGrid();
-*/
+
+
