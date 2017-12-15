9bb50b1cd884afed6834af1335b9f51f625a8605
sarden11037 <severo.7477@gmail.com>
2017-12-15 15:50:35 -0600

    Clear function
    added an attribute to clear the grid if the "clculate" button is selected again in the same session

--- design.js
+++ design.js
@@ -1,43 +1,38 @@
 //select color input ;
 let colorChoice;
 colorChoice = $("#colorPicker").val();
-//alert(colorChoice);
 //event listener for color choice 
 $("#colorPicker").change(function() {
-  colorChoice = $("#colorPicker").val();
-  });
+    colorChoice = $("#colorPicker").val();
+});
+//sets the new color choice
 $("#colorPicker").val("colorChoice");
-//select size input 
-//when size is submitted by the user, call make grid 
-/*
-$("submit-Size").change(function(e) {
-    e.preventDefault();
-});*/
-$("#submit-Size").click(function(e) { 
-                         makeGrid(e);
-                         });
+//event listener for the calculate button
+$("#submit-Size").click(function(e) {
+    makeGrid(e);
+});
+//function that gets called when button is clicked
 function makeGrid(e) {
- 
+    //prevent default called to prevent page
+    //from reloading
     e.preventDefault();
+    $("#pixel_canvas").empty();
+    //assign height and width values
     let height = $("#input_height").val();
     let width = $("#input_width").val();
-    for (let i = 0;i< height;i++)
-        {
-            $("#pixel_canvas").append("<tr></tr>");
-            for(let x = 0; x< width;x++)
-                {
-                    $("tr").last().append("<td></td>");
-                }
+    //loop called to build the grid
+    for (let i = 0; i < height; i++) {
+        $("#pixel_canvas").append("<tr></tr>");
+        for (let x = 0; x < width; x++) {
+            $("tr").last().append("<td></td>");
         }
-    
-  }
+    }
+
+}
+//event listener for the grid to paint the cell
 let click = $("#pixel_canvas").on("click", "td", function() {
-  
-  $(this).css("background-color", colorChoice);
+
+    $(this).css("background-color", colorChoice);
 });
 //was thinging about adding an if statement
 //for errors on clicking but was uneccsary
-    
-
-
-
